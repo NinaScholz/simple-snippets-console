b55a6f35084d0291d004e64a2b6b2ae68f08f4ad
T.J. Crowder <tj@crowdersoftware.com>
2015-05-04 09:56:28 +0100

    Add the ability to specify a tag

--- snippet.js
+++ snippet.js
@@ -1,21 +1,20 @@
 // MIT license, see: https://github.com/tjcrowder/simple-snippets-console/blob/master/LICENSE
 var snippet = {
-    version: "1.0",
+    version: "1.1",
 
     // Writes out the given text in a monospaced paragraph tag, escaping
     // & and < so they aren't rendered as HTML.
-    log: function(msg) {
+    log: function(msg, tag) {
+        var elm = document.createElement(tag || "p");
+        elm.style.fontFamily = "monospace";
+        elm.style.margin = "2px 0 2px 0";
         if (Object.prototype.toString.call(msg) === "[object Array]") {
             msg = msg.join();
         } else if (typeof object === "object") {
             msg = msg === null ? "null" : JSON.stringify(msg);
         }
-        document.body.insertAdjacentHTML(
-            'beforeend',
-            '<p style="font-family: monospace; margin: 2px 0 2px 0">' +
-            msg.replace(/&/g, '&amp;').replace(/</g, '&lt;') +
-            '</p>'
-        );
+        elm.innerHTML = msg.replace(/&/g, '&amp;').replace(/</g, '&lt;');
+        document.body.appendChild(elm);
     },
 
     // Writes out the given HTML at the end of the body,
